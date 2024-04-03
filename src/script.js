cb32abb3cbf87a2b5ffefd0f1c1bd25c285abed9
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-04-02 21:15:51 -0300

    Arquivo calculadora entregavel

--- src/script.js
+++ src/script.js
@@ -1,11 +1,52 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(numero1,numero2, operador){
+  let resultado 
+  let num1  = Number(numero1)
+  let num2 = Number (numero2)
+   switch (operador) {
+       case "+":
+               resultado = num1 + num2
+           break;
+       case "-":
+               resultado  = num1 - num2 
+           break;
+       case "*":
+               resultado = num1 * num2
+           break;
+
+       case "/":
+        if (num2 === 0) {
+          resultado = "Divisão por zero";
+      } else {
+          resultado = num1 / num2;
+      }
+           break;
+       case "e":
+        if (numero2 < 0) {
+          resultado = 1;
+          for (let i = 0; i < -numero2; i++) {
+              resultado /= numero1;
+          }
+      } else {
+          resultado = 1;
+          for (let i = 0; i < numero2; i++) {
+              resultado *= numero1;
+          }
+      }
+
+           break;
+
+       default:
+               resultado = ("Essa opção não existe ")
+           break;
+   }
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+return resultado 
+
+
+}
+
 
 let visor = document.getElementById("visor");
 
